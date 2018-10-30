936889f763ce8bc0652f7fd40f33bd6dcb872234
Alexey Loginov <loginov.alex.valer@gmail.com>
2018-10-30 07:20:43 +0000

    update qabcs_ru.ts from tx

--- langs/qabcs_ru.ts
+++ langs/qabcs_ru.ts
@@ -1,72 +1,55 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="ru_RU">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" version="2.1">
 <context>
     <name>FormAbout</name>
     <message>
         <location filename="../FormAbout.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Диалог</translation>
     </message>
     <message>
         <location filename="../FormAbout.ui" line="31"/>
         <source>qABCs</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../FormAbout.ui" line="52"/>
-        <source>Version: 1.0.0</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FormAbout.ui" line="73"/>
-        <source>Date build: 28.10.2018</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FormAbout.ui" line="94"/>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;© 2018 DanSoft. All rights reserved.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FormAbout.ui" line="115"/>
-        <source>Close</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FormAbout.ui" line="133"/>
-        <source>Visit web site</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FormAbout.ui" line="146"/>
-        <source>TextLabel</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../FormAbout.ui" line="167"/>
+        <location filename="../FormAbout.ui" line="131"/>
         <source>License: GPLv3+</source>
-        <translation type="unfinished"></translation>
+        <translation>Лицензия: GPLv3+</translation>
     </message>
     <message>
-        <location filename="../FormAbout.ui" line="183"/>
+        <location filename="../FormAbout.ui" line="190"/>
         <source>BitBucket</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="208"/>
+        <source>Visit web site</source>
+        <translation>Посетить веб-сайт</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.ui" line="226"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
     </message>
     <message>
         <location filename="../FormAbout.cpp" line="16"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>О программе</translation>
     </message>
     <message>
         <location filename="../FormAbout.cpp" line="19"/>
         <source>Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия:</translation>
     </message>
     <message>
         <location filename="../FormAbout.cpp" line="20"/>
         <source>Date build:</source>
-        <translation type="unfinished"></translation>
+        <translation>Дата сборки:</translation>
+    </message>
+    <message>
+        <location filename="../FormAbout.cpp" line="21"/>
+        <source>All rights reserved.</source>
+        <translation>Все права защищены.</translation>
     </message>
 </context>
 <context>
@@ -74,58 +57,103 @@
     <message>
         <location filename="../FormHelp.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Диалог</translation>
     </message>
     <message>
         <location filename="../FormHelp.ui" line="48"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>ОК</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="11"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Справка</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="15"/>
         <source>How to use:
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Как использовать:
+
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="16"/>
         <source>Selecting a mode:
-        The four icons on the left side of the toolbar represent the
-        different modes.  Use the &quot;Tab&quot; key to toggle between modes,
+        The six icons on the left side of the toolbar represent the
+        different modes. Use the &quot;Tab&quot; key to toggle between modes,
         or click on an icon with the mouse.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Выбор режима:
+        Шесть значков в левой части панели инструментов представляют
+        собой разные режимы. Используйте клавишу «Tab» для
+        переключения между режимами или щёлкните мышью по значку.
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="21"/>
         <source>Alphabet Mode:
-        The first icon on the left is the Alphabet Mode.  Press the
+        The first icon on the left is the Alphabet Mode. Press the
         letter on the keyboard that is displayed on the screen to
-        cycle through the English alphabet.  Press the &quot;Backspace&quot;
-        key to move back one letter.  Finish by finding all the
-        letters, A-Z.
+        cycle through alphabet. Press the &quot;Backspace&quot; key to move
+        back one letter. Finish by finding all the letters.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Режим алфавита:
+        Первый значок слева - это режим алфавита. Нажмите
+        букву на клавиатуре, которая отображается на экране,
+        для циклического прохождения по алфавиту. Нажмите
+        клавишу &quot;Backspace&quot;, чтобы переместиться назад на одну
+        букву. Закончите, найдя все буквы.
+</translation>
     </message>
     <message>
-        <location filename="../FormHelp.cpp" line="28"/>
+        <location filename="../FormHelp.cpp" line="27"/>
+        <source>Random Mode:
+        The second icon on the left is the Random Mode. Press the
+        letter on the keyboard that is displayed on the screen.
+        Press the &quot;Backspace&quot; key to move back one letter.
+        Finish by finding all the letters.
+</source>
+        <translation>Случайный режим:
+        Второй значок слева - это случайный режим. Нажмите
+        букву на клавиатуре, которая отображается на экране.
+        Нажмите клавишу &quot;Backspace&quot;, чтобы переместиться
+        назад на одну букву. Закончите, найдя все буквы.
+</translation>
+    </message>
+    <message>
+        <location filename="../FormHelp.cpp" line="33"/>
         <source>Other Modes:
         In all other modes, simply press a key on the keyboard to see
         a letter with a related image and description.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Другие режимы:
+        Во всех других режимах просто нажмите клавишу на клавиатуре,
+        чтобы увидеть букву с соответствующим изображением и описанием.
+</translation>
     </message>
     <message>
-        <location filename="../FormHelp.cpp" line="32"/>
+        <location filename="../FormHelp.cpp" line="37"/>
         <source>Sounds:
-        Press the spacebar to hear the name of the pictured object.</source>
-        <translation type="unfinished"></translation>
+        Press the spacebar to hear the name of the pictured object.
+        You can turn on/off sounds by pressing on the &quot;Sound on/off&quot;
+        icon.
+</source>
+        <translation>Звуки:
+        Нажмите пробел чтобы услышать название изображённого
+        объекта. Вы можете включать/выключать звуки, нажимая на
+        значке &quot;Включить/выключить звук&quot;.
+</translation>
+    </message>
+    <message>
+        <location filename="../FormHelp.cpp" line="42"/>
+        <source>Select ABC language:
+        You can change ABC language by pressing on the &quot;Select
+        language&quot; icon. Default language is English.</source>
+        <translation>Выбор языка алфавита:
+        Вы можете выбрать язык алфавита, нажав на значке &quot;Выбрать
+        язык&quot;. Языком по-умолчанию является английский.</translation>
     </message>
 </context>
 <context>
@@ -133,27 +161,27 @@
     <message>
         <location filename="../FormSelectLanguage.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Диалог</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.ui" line="31"/>
-        <source>Select language abc:</source>
-        <translation type="unfinished"></translation>
+        <source>Select ABC language:</source>
+        <translation>Выберите язык алфавита:</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.ui" line="54"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Отмена</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.ui" line="67"/>
-        <source>Ok</source>
-        <translation type="unfinished"></translation>
+        <source>OK</source>
+        <translation>ОК</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.cpp" line="19"/>
-        <source>Select language abc</source>
-        <translation type="unfinished"></translation>
+        <source>Select ABC language</source>
+        <translation>Выберите язык алфавита</translation>
     </message>
 </context>
 <context>
@@ -163,138 +191,138 @@
         <location filename="../mainwindow.cpp" line="95"/>
         <location filename="../mainwindow.cpp" line="99"/>
         <location filename="../mainwindow.cpp" line="100"/>
-        <location filename="../mainwindow.cpp" line="528"/>
-        <location filename="../mainwindow.cpp" line="546"/>
+        <location filename="../mainwindow.cpp" line="531"/>
+        <location filename="../mainwindow.cpp" line="549"/>
         <source>Find the letter on the keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Найти букву на клавиатуре</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="104"/>
         <location filename="../mainwindow.cpp" line="105"/>
-        <source>Show foods that begin with each letter</source>
-        <translation type="unfinished"></translation>
+        <source>Show foods for each letter</source>
+        <translation>Показать продукты для каждой буквы</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="109"/>
         <location filename="../mainwindow.cpp" line="110"/>
-        <source>Show animals that begin with each letter</source>
-        <translation type="unfinished"></translation>
+        <source>Show animals for each letter</source>
+        <translation>Показать животных для каждой буквы</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="114"/>
         <location filename="../mainwindow.cpp" line="115"/>
         <source>Show musical instruments for each letter</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать музыкальные инструменты для каждой буквы</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="119"/>
         <location filename="../mainwindow.cpp" line="120"/>
         <source>Show toys for each letter</source>
-        <translation type="unfinished"></translation>
+        <translation>Показать игрушки для каждой буквы</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="124"/>
         <location filename="../mainwindow.cpp" line="125"/>
         <source>Sound on/off</source>
-        <translation type="unfinished"></translation>
+        <translation>Включить/выключить звук</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="127"/>
         <location filename="../mainwindow.cpp" line="128"/>
         <source>Select language</source>
-        <translation type="unfinished"></translation>
+        <translation>Выбрать язык</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="130"/>
         <location filename="../mainwindow.cpp" line="131"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Справка</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="133"/>
         <location filename="../mainwindow.cpp" line="134"/>
         <source>About qABCs</source>
-        <translation type="unfinished"></translation>
+        <translation>О qABCs</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="136"/>
         <location filename="../mainwindow.cpp" line="137"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Выход</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="198"/>
         <location filename="../mainwindow.cpp" line="242"/>
-        <source>Error open </source>
-        <translation type="unfinished"></translation>
+        <source>Error while opening</source>
+        <translation>Ошибка при открытии</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="206"/>
         <source>is not valid</source>
-        <translation type="unfinished"></translation>
+        <translation>не является допустимым</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="289"/>
-        <source>error str: </source>
-        <translation type="unfinished"></translation>
+        <source>Error str:</source>
+        <translation>Строка с ошибкой:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="476"/>
-        <location filename="../mainwindow.cpp" line="500"/>
+        <location filename="../mainwindow.cpp" line="473"/>
+        <location filename="../mainwindow.cpp" line="503"/>
         <source>CONGRATS!</source>
-        <translation type="unfinished"></translation>
+        <translation>ПОЗДРАВЛЯЕМ!</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="477"/>
-        <location filename="../mainwindow.cpp" line="501"/>
+        <location filename="../mainwindow.cpp" line="474"/>
+        <location filename="../mainwindow.cpp" line="504"/>
         <source>Press &quot;ENTER&quot; to Play Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Нажмите &quot;ENTER&quot; чтобы сыграть снова</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="537"/>
-        <location filename="../mainwindow.cpp" line="585"/>
-        <source>abc is not loaded</source>
-        <translation type="unfinished"></translation>
+        <location filename="../mainwindow.cpp" line="540"/>
+        <location filename="../mainwindow.cpp" line="591"/>
+        <source>ABC is not loaded</source>
+        <translation>Алфавит не загружен</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="594"/>
+        <location filename="../mainwindow.cpp" line="600"/>
         <source>Press a Key to See an Animal</source>
-        <translation type="unfinished"></translation>
+        <translation>Нажмите клавишу чтобы увидеть животное</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="602"/>
+        <location filename="../mainwindow.cpp" line="608"/>
         <source>Animals</source>
-        <translation type="unfinished"></translation>
+        <translation>Животные</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="607"/>
+        <location filename="../mainwindow.cpp" line="613"/>
         <source>Press a Key to See a Food</source>
-        <translation type="unfinished"></translation>
+        <translation>Нажмите клавишу чтобы увидеть продукт</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="615"/>
+        <location filename="../mainwindow.cpp" line="621"/>
         <source>Food</source>
-        <translation type="unfinished"></translation>
+        <translation>Продукты</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="620"/>
+        <location filename="../mainwindow.cpp" line="626"/>
         <source>Press a Key to See an Instrument</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="628"/>
-        <source>Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Нажмите клавишу чтобы увидеть инструмент</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="634"/>
-        <source>Press a Key to See a Toy</source>
-        <translation type="unfinished"></translation>
+        <source>Music</source>
+        <translation>Музыка</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="642"/>
+        <location filename="../mainwindow.cpp" line="640"/>
+        <source>Press a Key to See a Toy</source>
+        <translation>Нажмите клавишу чтобы увидеть игрушку</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="648"/>
         <source>Toys</source>
-        <translation type="unfinished"></translation>
+        <translation>Игрушки</translation>
     </message>
 </context>
 </TS>
