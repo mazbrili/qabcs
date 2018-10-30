8991b0719e24b2042af77c24b83abb55707fda7c
Alexey Loginov <loginov.alex.valer@gmail.com>
2018-10-30 02:06:35 +0000

    Update Help

--- FormHelp.cpp
+++ FormHelp.cpp
@@ -14,23 +14,34 @@ FormHelp::FormHelp(QWidget *parent) :
 
     ui->textEdit->setText(tr("How to use:\n\n")+
       tr("Selecting a mode:\n\
-        The four icons on the left side of the toolbar represent the\n\
-        different modes.  Use the \"Tab\" key to toggle between modes,\n\
+        The six icons on the left side of the toolbar represent the\n\
+        different modes. Use the \"Tab\" key to toggle between modes,\n\
         or click on an icon with the mouse.\n")+
     "\n"+
       tr("Alphabet Mode:\n\
-        The first icon on the left is the Alphabet Mode.  Press the\n\
+        The first icon on the left is the Alphabet Mode. Press the\n\
         letter on the keyboard that is displayed on the screen to\n\
-        cycle through the English alphabet.  Press the \"Backspace\"\n\
-        key to move back one letter.  Finish by finding all the\n\
-        letters, A-Z.\n")+
+        cycle through alphabet. Press the \"Backspace\" key to move\n\
+        back one letter. Finish by finding all the letters.\n")+
+    "\n"+
+      tr("Random Mode:\n\
+        The second icon on the left is the Random Mode. Press the\n\
+        letter on the keyboard that is displayed on the screen.\n\
+        Press the \"Backspace\" key to move back one letter.\n\
+        Finish by finding all the letters.\n")+
     "\n"+
       tr("Other Modes:\n\
         In all other modes, simply press a key on the keyboard to see\n\
         a letter with a related image and description.\n")+
     "\n"+
       tr("Sounds:\n\
-        Press the spacebar to hear the name of the pictured object.")
+        Press the spacebar to hear the name of the pictured object.\n\
+        You can turn on/off sounds by pressing on the \"Sound on/off\"\n\
+        icon.\n")+
+    "\n"+
+      tr("Select ABC language:\n\
+        You can change ABC language by pressing on the \"Select\n\
+        language\" icon. Default language is English.")
     );
 
 
