59caae419f47917e1ccc3843f23fbbf46c0a4b92
sasha <dik@inbox.ru>
2018-11-14 17:59:41 +0900

    Revert "revert 2 commits"
    This reverts commit 2b46b2b8e2641749a29f61d10eb2226c12888e95.

--- mainwindow.h
+++ mainwindow.h
@@ -112,6 +112,7 @@ class MainWindow : public QMainWindow {
 
         QString _speak_method;
         QString _espeak_params;
+        bool _disable_additional_keys;
         QString _lastFormatLoaded;
         QVector<LETTER_INFO> listLetters;
 
