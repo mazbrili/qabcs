263827035561bdd3f57ecebeaf395dff8f5647ea
glixx <alexl@mageia.org>
2019-07-11 03:56:30 +0300

    update Polish

--- langs/qabcs_pl.ts
+++ langs/qabcs_pl.ts
@@ -4,7 +4,7 @@
     <message>
         <location filename="../FormAbout.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"/>
+        <translation>Okno dialogowe</translation>
     </message>
     <message>
         <location filename="../FormAbout.ui" line="131"/>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../FormAbout.ui" line="208"/>
         <source>Visit web site</source>
-        <translation type="unfinished"/>
+        <translation>Odwiedź stronę internetową</translation>
     </message>
     <message>
         <location filename="../FormAbout.ui" line="226"/>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../FormAbout.cpp" line="20"/>
         <source>Date build:</source>
-        <translation type="unfinished"/>
+        <translation>Data kompilacji:</translation>
     </message>
     <message>
         <location filename="../FormAbout.cpp" line="21"/>
         <source>All rights reserved.</source>
-        <translation type="unfinished"/>
+        <translation>Wszelkie prawa zastrzeżone.</translation>
     </message>
 </context>
 <context>
@@ -47,12 +47,12 @@
     <message>
         <location filename="../FormHelp.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"/>
+        <translation>Okno dialogowe</translation>
     </message>
     <message>
         <location filename="../FormHelp.ui" line="48"/>
         <source>OK</source>
-        <translation type="unfinished"/>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="12"/>
@@ -64,7 +64,9 @@
         <source>How to use:
 
 </source>
-        <translation type="unfinished"/>
+        <translation>Jak używać:
+
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="17"/>
@@ -73,7 +75,11 @@
         different modes. Use the &quot;Tab&quot; key to toggle between modes,
         or click on an icon with the mouse.
 </source>
-        <translation type="unfinished"/>
+        <translation>Wybieranie trybu:
+        Sześć ikon po lewej stronie paska narzędzi reprezentuje
+        różne tryby. Użyj klawisza „Tab”, aby przełączać się między trybami,
+        lub kliknij na ikonę za pomocą myszy.
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="22"/>
@@ -83,7 +89,12 @@
         cycle through alphabet. Press the &quot;Backspace&quot; key to move
         back one letter. Finish by finding all the letters.
 </source>
-        <translation type="unfinished"/>
+        <translation>Tryb Alfabet:
+        Pierwsza ikona po lewej to Tryb Alfabet. Naciśnij literę
+        na klawiaturze, która jest wyświetlana na ekranie,
+        aby przejść przez alfabet. Naciśnij klawisz „Backspace”,
+        aby cofnąć się o jedną literę. Zakończ, znajdując wszystkie litery.
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="28"/>
@@ -93,7 +104,12 @@
         Press the &quot;Backspace&quot; key to move back one letter.
         Finish by finding all the letters.
 </source>
-        <translation type="unfinished"/>
+        <translation>Tryb Losowy:
+        Druga ikona po lewej to tryb losowy. Naciśnij literę
+        na klawiaturze wyświetlanej na ekranie. Naciśnij klawisz
+        „Backspace” aby cofnąć się o jedną literę.
+        Zakończ, znajdując wszystkie litery.
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="34"/>
@@ -101,7 +117,10 @@
         In all other modes, simply press a key on the keyboard to see
         a letter with a related image and description.
 </source>
-        <translation type="unfinished"/>
+        <translation>Inne Tryby:
+        We wszystkich innych trybach wystarczy nacisnąć klawisz na klawiaturze,
+        aby zobaczyć literę z powiązanym obrazem i opisem.
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="38"/>
@@ -110,7 +129,11 @@
         You can turn on/off sounds by pressing on the &quot;Sound on/off&quot;
         icon.
 </source>
-        <translation type="unfinished"/>
+        <translation>Dźwięki:
+        Naciśnij spację, aby usłyszeć nazwę obiektu pokazanego na zdjęciu.
+        Dźwięki można włączyć / wyłączyć, naciskając ikonę
+        „Dźwięk włącz/wyłącz”
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="43"/>
@@ -118,14 +141,19 @@
         You can change ABC language by pressing on the &quot;Select
         language&quot; icon. Default language is American English.
 </source>
-        <translation type="unfinished"/>
+        <translation>Wybierz język ABC:
+        Możesz zmienić język ABC, naciskając ikonę „Wybierz język”.
+        Domyślnym językiem jest amerykański angielski.
+</translation>
     </message>
     <message>
         <location filename="../FormHelp.cpp" line="47"/>
         <source>Typing on/off:
         You can switch from typing of letters on using left/right keys
         instead.</source>
-        <translation type="unfinished"/>
+        <translation>Wpisywanie włącz/wyłącz:
+        Możesz przełączać się między wpisywaniem liter używając klawiszy
+        lewo/prawo.</translation>
     </message>
 </context>
 <context>
@@ -133,12 +161,12 @@
     <message>
         <location filename="../FormSelectLanguage.ui" line="14"/>
         <source>Dialog</source>
-        <translation type="unfinished"/>
+        <translation>Okno dialogowe</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.ui" line="31"/>
         <source>Select ABC language:</source>
-        <translation type="unfinished"/>
+        <translation>Wybierz język ABC:</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.ui" line="54"/>
@@ -148,12 +176,12 @@
     <message>
         <location filename="../FormSelectLanguage.ui" line="67"/>
         <source>OK</source>
-        <translation type="unfinished"/>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.ui" line="85"/>
         <source>Select GUI language:</source>
-        <translation type="unfinished"/>
+        <translation>Wybierz język GUI:</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.cpp" line="20"/>
@@ -163,12 +191,12 @@
     <message>
         <location filename="../FormSelectLanguage.cpp" line="72"/>
         <source>Error while opening</source>
-        <translation type="unfinished"/>
+        <translation>Błąd podczas otwierania</translation>
     </message>
     <message>
         <location filename="../FormSelectLanguage.cpp" line="79"/>
         <source>is not valid</source>
-        <translation type="unfinished"/>
+        <translation>nie jest prawidłowy</translation>
     </message>
 </context>
 <context>
@@ -176,67 +204,67 @@
     <message>
         <location filename="../mainwindow.cpp" line="54"/>
         <source>Using path:</source>
-        <translation type="unfinished"/>
+        <translation>Użyj ścieżki:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="69"/>
         <source>the play command from the sox package was not found. Sound will be muted.</source>
-        <translation type="unfinished"/>
+        <translation>komenda odtwarzania z pakietu sox nie została znaleziona. Dźwięk zostanie wyciszony.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="76"/>
         <source>espeak was not found. Some features will not be available.</source>
-        <translation type="unfinished"/>
+        <translation>nie znaleziono mowy. Niektóre funkcje nie będą dostępne.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="131"/>
         <location filename="../mainwindow.cpp" line="132"/>
         <location filename="../mainwindow.cpp" line="853"/>
         <source>Find the letter on the keyboard (alphabetically)</source>
-        <translation type="unfinished"/>
+        <translation>Znajdź literę na klawiaturze (alfabetycznie)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="136"/>
         <location filename="../mainwindow.cpp" line="137"/>
         <location filename="../mainwindow.cpp" line="873"/>
         <source>Find the letter on the keyboard (randomly)</source>
-        <translation type="unfinished"/>
+        <translation>Znajdź literę na klawiaturze (losowo)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="141"/>
         <location filename="../mainwindow.cpp" line="142"/>
         <source>Show foods for each letter</source>
-        <translation type="unfinished"/>
+        <translation>Pokaż żywność dla każdej litery</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="146"/>
         <location filename="../mainwindow.cpp" line="147"/>
         <source>Show animals for each letter</source>
-        <translation type="unfinished"/>
+        <translation>Pokaż zwierząta dla każdej litery</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="151"/>
         <location filename="../mainwindow.cpp" line="152"/>
         <source>Show musical instruments for each letter</source>
-        <translation type="unfinished"/>
+        <translation>Pokaż instrumenty muzyczne dla każdej litery</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="156"/>
         <location filename="../mainwindow.cpp" line="157"/>
         <source>Show toys for each letter</source>
-        <translation type="unfinished"/>
+        <translation>Pokaż zabawki dla każdej litery</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="161"/>
         <location filename="../mainwindow.cpp" line="162"/>
         <source>Sound on/off</source>
-        <translation type="unfinished"/>
+        <translation>Dźwięk włącz/wyłącz</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="164"/>
         <location filename="../mainwindow.cpp" line="165"/>
         <source>Typing on/off</source>
-        <translation type="unfinished"/>
+        <translation>Wpisywanie włącz/wyłącz</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="167"/>
@@ -260,35 +288,35 @@
         <location filename="../mainwindow.cpp" line="176"/>
         <location filename="../mainwindow.cpp" line="177"/>
         <source>Exit</source>
-        <translation>Wyjście</translation>
+        <translation>Zamknij</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="319"/>
         <location filename="../mainwindow.cpp" line="392"/>
         <source>Error while opening</source>
-        <translation type="unfinished"/>
+        <translation>Błąd podczas otwierania</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="330"/>
         <source>is not valid</source>
-        <translation type="unfinished"/>
+        <translation>nie jest prawidłowy</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="476"/>
         <source>Error str:</source>
-        <translation type="unfinished"/>
+        <translation>Błąd str:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="753"/>
         <location filename="../mainwindow.cpp" line="801"/>
         <source>CONGRATS!</source>
-        <translation type="unfinished"/>
+        <translation>GRATULACJE!</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="754"/>
         <location filename="../mainwindow.cpp" line="802"/>
         <source>Press &quot;ENTER&quot; to Play Again</source>
-        <translation type="unfinished"/>
+        <translation>Naciśnij &quot;ENTER&quot; aby ponownie grać</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="862"/>
@@ -298,32 +326,32 @@
         <location filename="../mainwindow.cpp" line="975"/>
         <location filename="../mainwindow.cpp" line="995"/>
         <source>ABC is not loaded</source>
-        <translation type="unfinished"/>
+        <translation>ABC nie jest załadowane</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="925"/>
         <source>Press a Key to See an Animal</source>
-        <translation type="unfinished"/>
+        <translation>Naciśnij klawisz, aby zobaczyć Zwierzę</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="939"/>
         <source>Animals</source>
-        <translation type="unfinished"/>
+        <translation>Zwierzęta</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="945"/>
         <source>Press a Key to See a Food</source>
-        <translation type="unfinished"/>
+        <translation>Naciśnij klawisz, aby zobaczyć Jedzenie</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="961"/>
         <source>Food</source>
-        <translation type="unfinished"/>
+        <translation>Jedzenie</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="966"/>
         <source>Press a Key to See an Instrument</source>
-        <translation type="unfinished"/>
+        <translation>Naciśnij klawisz, aby zobaczyć Instrument</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="980"/>
@@ -333,22 +361,22 @@
     <message>
         <location filename="../mainwindow.cpp" line="986"/>
         <source>Press a Key to See a Toy</source>
-        <translation type="unfinished"/>
+        <translation>Naciśnij klawisz, aby zobaczyć Zabawkę</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1000"/>
         <source>Toys</source>
-        <translation type="unfinished"/>
+        <translation>Zabawki</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1063"/>
         <source>play was found:</source>
-        <translation type="unfinished"/>
+        <translation>znaleziono grę:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="1089"/>
         <source>espeak was found:</source>
-        <translation type="unfinished"/>
+        <translation>znaleziono espeak:</translation>
     </message>
 </context>
 <context>
@@ -357,12 +385,12 @@
         <location filename="../LoaderAbcFormats.cpp" line="35"/>
         <location filename="../LoaderAbcFormats.cpp" line="119"/>
         <source>Error while opening</source>
-        <translation type="unfinished"/>
+        <translation>Błąd podczas otwierania</translation>
     </message>
     <message>
         <location filename="../LoaderAbcFormats.cpp" line="49"/>
         <source>is not valid</source>
-        <translation type="unfinished"/>
+        <translation>nie jest prawidłowy</translation>
     </message>
 </context>
 </TS>
