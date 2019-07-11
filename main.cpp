67c07ec4605a4a11db934b2f2ab98dabd741edf4
sasha <dik@inbox.ru>
2019-07-11 16:52:26 +0900

    output version

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 #include <QDir>
 #include <QTextCodec>
 #include <QTime>
+#include <QCommandLineParser>
 
+#include "config_qabcs.h"
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
+
+    QApplication::setApplicationName("qABCs");
+    QApplication::setApplicationVersion(QABCS_VERSION);
+    QApplication::setOrganizationName("DanSoft");
+    QApplication::setOrganizationDomain("dansoft.ru");
+
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.process(app);
+
 
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     QDir::setCurrent(QApplication::applicationDirPath());
