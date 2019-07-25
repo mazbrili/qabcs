3346802644a7021132bd2f70f31aad1ce179c010
Alexey Loginov <alexl@mageia.org>
2019-07-25 11:26:24 +0300

    using http://translate.google.com/translate_tts too

--- scripts/google.sh
+++ scripts/google.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 # Author: Alexey Loginov
 # Licence: GPLv3+
-# Description: convert text and letters into .ogg (vorbis) using Google Cloud Text-to-Speech
+# Description: convert text and letters into .ogg (vorbis) using Google Cloud Text-to-Speech or Text-to-Speech
 # https://cloud.google.com/text-to-speech/docs
-# You need curl, ffmpeg installed
+# You need curl, ffmpeg or wget installed
 
 key="$1"
 
@@ -23,6 +23,8 @@ key="$1"
 #lang0="en"
 # file format: properties or json
 #file_format="properties"
+# using Google Cloud Text-to-Speech API or not
+#api=true
 
 lang="ru"
 
@@ -31,26 +33,31 @@ case $lang in
        languageCode="en-US"
        name="en-US-Standard-E"
        ssmlGender="FEMALE"
+       api=true
        ;;
      nl)
        languageCode="nl-NL"
        name="nl-NL-Standard-A"
        ssmlGender="FEMALE"
+       api=true
        ;;
      ru)
        languageCode="ru-RU"
        name="ru-RU-Standard-A"
        ssmlGender="FEMALE"
+       api=true
        ;;
      uk)
        languageCode="uk-UA"
        name="uk-UA-Standard-A"
        ssmlGender="FEMALE"
+       api=true
        ;;
      de)
        languageCode="de-DE"
        name="de-DE-Standard-B"
        ssmlGender="MALE"
+       api=true
        ;;
      *)
        echo "Unknown language!"
@@ -58,54 +65,106 @@ case $lang in
        ;;
 esac
 
+get_sound(){
+if [ "$api" = "true" ]
+then
+  curl -H "X-Goog-Api-Key: $key" \
+    -H "Content-Type: application/json; charset=utf-8" \
+    --data "{
+      'input':{
+        'text':'$text'
+      },
+      'voice':{
+        'languageCode':'$languageCode',
+        'name':'$name',
+        'ssmlGender':'$ssmlGender'
+      },
+      'audioConfig':{
+        'audioEncoding':'$audioEncoding'
+      }
+    }" "https://texttospeech.googleapis.com/v1/text:synthesize" > synthesize-text.txt
+  echo `cat synthesize-text.txt | grep audioContent|cut -d ":" --fields=2|cut -d "\"" --fields=2` > synthesize-output-base64.txt
+  base64 synthesize-output-base64.txt --decode > ../abcs/$lang0/sounds/$1/"$filename.$format"
+  rm -f synthesize-text.txt synthesize-output-base64.txt
+  ffmpeg -i ../abcs/$lang0/sounds/$1/"$filename.$format" -acodec libvorbis ../abcs/$lang0/sounds/$1/"$filename.$output"
+else
+  wget "http://translate.google.com/translate_tts?tl=$lang&ie=UTF-8&q=$text&client=tw-ob" -U Mozilla/5.0 -O ../abcs/$lang0/sounds/$1/"$filename.$output"
+fi
+if [ ! -f ../abcs/$lang0/sounds/$1/"$filename.$output" ]
+then
+  echo "Error while creating "$filename.$output""
+  rm -f ../abcs/$lang0/sounds/$1/*.$format
+  exit 1
+fi
+}
+
 lang0=$lang
 audioEncoding="LINEAR16"
-format="wav"
-output="ogg"
+if [ "$api" = "true" ]
+then
+  format="wav"
+  output="ogg"
+else
+  format="wav"
+  output="mp3"
+fi
 file_format="properties"
 
-if [ -z "$key" ]
+if [ "$api" = "true" ]
 then
-   echo "Parameter 'key' is mandatory!"
-   exit 1
-fi
+  if [ -z "$key" ]
+  then
+     echo "Parameter 'key' is mandatory!"
+     exit 1
+  fi
 
-curl=`curl -h`
+  curl=`curl -h`
 
-if [ -z "$curl" ]
-then
-   echo "You have no curl installed!"
-   exit 1
+  if [ -z "$curl" ]
+  then
+     echo "You have no curl installed!"
+     exit 1
+  else
+     echo "curl detected"
+  fi
+
+  ffmpeg=`ffmpeg -version`
+
+  if [ -z "$ffmpeg" ]
+  then
+     echo "You have no ffmpeg installed!"
+     exit 1
+  else
+     echo "ffmpeg detected"
+  fi
 else
-   echo "curl detected"
-fi
+  wget=`wget -h`
 
-ffmpeg=`ffmpeg -version`
-
-if [ -z "$ffmpeg" ]
-then
-   echo "You have no ffmpeg installed!"
-   exit 1
-else
-   echo "ffmpeg detected"
+  if [ -z "$wget" ]
+  then
+     echo "You have no wget installed!"
+     exit 1
+  else
+     echo "wget detected"
+  fi
 fi
 
 if [ -f "../abcs/$lang0/abc.json" ]
 then
-if [ $file_format = "json" ]
-then
-words_list=`cat ../abcs/$lang0/abc.json|grep \"name\"|cut -d ":" --fields=2 |cut -d "\"" --fields=2|awk '{print tolower($0)}'|sed "s| |yyy|g"|sort -u`
-letters_list=`cat ../abcs/$lang0/abc.json|grep -v espeak_words|grep -v visible|grep -v sound_letter|grep -v general|grep -v language |grep -v author|grep -v speak_method |grep -v espeak_params| grep -v letters | grep -v misc |grep -v pic | grep -v name |grep -v noises |grep -v food | grep -v animals | grep -v instrument | grep -v toys | grep -v inheritsFrom |awk '{print tolower($0)}'|sort -u | cut -d "\"" --fields=2|grep -v '\{'|grep -v '\}' | grep -v '\[' |grep -v '\]'| sed s/' '//g|grep -v "^$"`
-fi
+  if [ $file_format = "json" ]
+  then
+    words_list=`cat ../abcs/$lang0/abc.json|grep \"name\"|cut -d ":" --fields=2 |cut -d "\"" --fields=2|awk '{print tolower($0)}'|sed "s| |yyy|g"|sort -u`
+    letters_list=`cat ../abcs/$lang0/abc.json|grep -v espeak_words|grep -v visible|grep -v sound_letter|grep -v general|grep -v language |grep -v author|grep -v speak_method |grep -v espeak_params| grep -v letters | grep -v misc |grep -v pic | grep -v name |grep -v noises |grep -v food | grep -v animals | grep -v instrument | grep -v toys | grep -v inheritsFrom |awk '{print tolower($0)}'|sort -u | cut -d "\"" --fields=2|grep -v '\{'|grep -v '\}' | grep -v '\[' |grep -v '\]'| sed s/' '//g|grep -v "^$"`
+  fi
 fi
 
 if [ -f "../abcs/$lang0/abc.properties" ]
 then
-if [ $file_format = "properties" ]
-then
-words_list=`cat ../abcs/$lang0/abc.properties|grep -v '\[missing\]'|grep -v language|grep -v visible| grep -v inheritsFrom | grep -v author|grep -v espeak_params| grep -v "#"|cut -d "=" --fields=2 |awk '{print tolower($0)}'|sed "s| |yyy|g"|sort -u`
-letters_list=`cat ../abcs/$lang0/abc.properties|grep -v language|grep -v visible| grep -v inheritsFrom | grep -v author|grep -v espeak_params| grep -v "#"|cut -d "=" --fields=1 | cut -d ":" --fields=2 |awk '{print tolower($0)}'|sed "s| |yyy|g"|sort -u`
-fi
+  if [ $file_format = "properties" ]
+  then
+    words_list=`cat ../abcs/$lang0/abc.properties|grep -v '\[missing\]'|grep -v language|grep -v visible| grep -v inheritsFrom | grep -v author|grep -v espeak_params| grep -v "#"|cut -d "=" --fields=2 |awk '{print tolower($0)}'|sed "s| |yyy|g"|sort -u`
+    letters_list=`cat ../abcs/$lang0/abc.properties|grep -v language|grep -v visible| grep -v inheritsFrom | grep -v author|grep -v espeak_params| grep -v "#"|cut -d "=" --fields=1 | cut -d ":" --fields=2 |awk '{print tolower($0)}'|sed "s| |yyy|g"|sort -u`
+  fi
 fi
 
 if [ -z "$words_list" ]
@@ -126,10 +185,10 @@ rm -f ../abcs/$lang0/sounds/words/*.$output
 
 for a in $words_list
 do
-filename=`echo "$a"|cut -d "=" --fields=1|sed 's|yyy| |g'|sed 's| |_|'`
-text=`echo "$a"|cut -d "=" --fields=1|sed 's|yyy| |g'|sed 's|_| |'`
-# adding ◌́  for stress, - for pause, fixing pronounce
-case $lang in
+  filename=`echo "$a"|cut -d "=" --fields=1|sed 's|yyy| |g'|sed 's| |_|'`
+  text=`echo "$a"|cut -d "=" --fields=1|sed 's|yyy| |g'|sed 's|_| |'`
+  # adding ◌́  for stress, - for pause, fixing pronounce
+  case $lang in
      en)
        text=`echo "$text"|sed "s|ipu|eepoo|g"`
        ;;
@@ -150,32 +209,8 @@ case $lang in
        text=`echo "$text"|sed "s|подъёлочник|подъё́лочник|g"`
        text=`echo "$text"|sed "s|цуккини|цукини|g"`
        ;;
-esac
-curl -H "X-Goog-Api-Key: $key" \
-  -H "Content-Type: application/json; charset=utf-8" \
-  --data "{
-    'input':{
-      'text':'$text'
-    },
-    'voice':{
-      'languageCode':'$languageCode',
-      'name':'$name',
-      'ssmlGender':'$ssmlGender'
-    },
-    'audioConfig':{
-      'audioEncoding':'$audioEncoding'
-    }
-  }" "https://texttospeech.googleapis.com/v1/text:synthesize" > synthesize-text.txt
-echo `cat synthesize-text.txt | grep audioContent|cut -d ":" --fields=2|cut -d "\"" --fields=2` > synthesize-output-base64.txt
-base64 synthesize-output-base64.txt --decode > ../abcs/$lang0/sounds/words/"$filename.$format"
-rm -f synthesize-text.txt synthesize-output-base64.txt
-ffmpeg -i ../abcs/$lang0/sounds/words/"$filename.$format" -acodec libvorbis ../abcs/$lang0/sounds/words/"$filename.$output"
-if [ ! -f ../abcs/$lang0/sounds/words/"$filename.$output" ]
-then
-  echo "Error while creating "$filename.$output""
-  rm -f ../abcs/$lang0/sounds/words/*.$format
-  exit 1
-fi
+  esac
+  get_sound "words"
 done
 
 mkdir -p ../abcs/$lang0/sounds/alpha
@@ -184,133 +219,40 @@ rm -f ../abcs/$lang0/sounds/alpha/*.$output
 
 for a in $letters_list
 do
-filename="$a"
-text="$a"
-curl -H "X-Goog-Api-Key: $key" \
-  -H "Content-Type: application/json; charset=utf-8" \
-  --data "{
-    'input':{
-      'text':'$text'
-    },
-    'voice':{
-      'languageCode':'$languageCode',
-      'name':'$name',
-      'ssmlGender':'$ssmlGender'
-    },
-    'audioConfig':{
-      'audioEncoding':'$audioEncoding'
-    }
-  }" "https://texttospeech.googleapis.com/v1/text:synthesize" > synthesize-text.txt
-echo `cat synthesize-text.txt | grep audioContent|cut -d ":" --fields=2|cut -d "\"" --fields=2` > synthesize-output-base64.txt
-base64 synthesize-output-base64.txt --decode > ../abcs/$lang0/sounds/alpha/"$filename.$format"
-rm -f synthesize-text.txt synthesize-output-base64.txt
-ffmpeg -i ../abcs/$lang0/sounds/alpha/"$filename.$format" -acodec libvorbis ../abcs/$lang0/sounds/alpha/"$filename.$output"
-if [ ! -f ../abcs/$lang0/sounds/alpha/"$filename.$output" ]
-then
-  echo "Error while creating "$filename.$output""
-  rm -f ../abcs/$lang0/sounds/alpha/*.$format
-  exit 1
-fi
+  filename="$a"
+  text="$a"
+  get_sound "alpha"
 done
 
 # sometimes using other languages instead of native because of better pronounce
 if [ "$lang" = "ru" ]
 then
+    lang="uk"
     languageCode="uk-UA"
     name="uk-UA-Standard-A"
     filename="ы"
     text="и"
     rm -f ../abcs/$lang0/sounds/alpha/$filename.$format
     rm -f ../abcs/$lang0/sounds/alpha/$filename.$output
-    curl -H "X-Goog-Api-Key: $key" \
-    -H "Content-Type: application/json; charset=utf-8" \
-    --data "{
-        'input':{
-        'text':'$text'
-        },
-        'voice':{
-        'languageCode':'$languageCode',
-        'name':'$name',
-        'ssmlGender':'$ssmlGender'
-        },
-        'audioConfig':{
-        'audioEncoding':'$audioEncoding'
-        }
-    }" "https://texttospeech.googleapis.com/v1/text:synthesize" > synthesize-text.txt
-    echo `cat synthesize-text.txt | grep audioContent|cut -d ":" --fields=2|cut -d "\"" --fields=2` > synthesize-output-base64.txt
-    base64 synthesize-output-base64.txt --decode > ../abcs/$lang0/sounds/alpha/"$filename.$format"
-    rm -f synthesize-text.txt synthesize-output-base64.txt
-    ffmpeg -i ../abcs/$lang0/sounds/alpha/"$filename.$format" -acodec libvorbis ../abcs/$lang0/sounds/alpha/"$filename.$output"
-    if [ ! -f ../abcs/$lang0/sounds/alpha/"$filename.$output" ]
-    then
-      echo "Error while creating "$filename.$output""
-      rm -f ../abcs/$lang0/sounds/alpha/*.$format
-      exit 1
-    fi
+    get_sound "alpha"
 
+    lang="uk"
     languageCode="uk-UA"
     name="uk-UA-Standard-A"
     filename="ы"
     text="и"
     rm -f ../abcs/$lang0/sounds/words/$filename.$format
     rm -f ../abcs/$lang0/sounds/words/$filename.$output
-    curl -H "X-Goog-Api-Key: $key" \
-    -H "Content-Type: application/json; charset=utf-8" \
-    --data "{
-        'input':{
-        'text':'$text'
-        },
-        'voice':{
-        'languageCode':'$languageCode',
-        'name':'$name',
-        'ssmlGender':'$ssmlGender'
-        },
-        'audioConfig':{
-        'audioEncoding':'$audioEncoding'
-        }
-    }" "https://texttospeech.googleapis.com/v1/text:synthesize" > synthesize-text.txt
-    echo `cat synthesize-text.txt | grep audioContent|cut -d ":" --fields=2|cut -d "\"" --fields=2` > synthesize-output-base64.txt
-    base64 synthesize-output-base64.txt --decode > ../abcs/$lang0/sounds/words/"$filename.$format"
-    rm -f synthesize-text.txt synthesize-output-base64.txt
-    ffmpeg -i ../abcs/$lang0/sounds/words/"$filename.$format" -acodec libvorbis ../abcs/$lang0/sounds/words/"$filename.$output"
-    if [ ! -f ../abcs/$lang0/sounds/words/"$filename.$output" ]
-    then
-      echo "Error while creating "$filename.$output""
-      rm -f ../abcs/$lang0/sounds/words/*.$format
-      exit 1
-    fi
+    get_sound "words"
 
+    lang="uk"
     languageCode="uk-UA"
     name="uk-UA-Standard-A"
     filename="ыых"
     text="и-их"
     rm -f ../abcs/$lang0/sounds/words/$filename.$format
     rm -f ../abcs/$lang0/sounds/words/$filename.$output
-    curl -H "X-Goog-Api-Key: $key" \
-    -H "Content-Type: application/json; charset=utf-8" \
-    --data "{
-        'input':{
-        'text':'$text'
-        },
-        'voice':{
-        'languageCode':'$languageCode',
-        'name':'$name',
-        'ssmlGender':'$ssmlGender'
-        },
-        'audioConfig':{
-        'audioEncoding':'$audioEncoding'
-        }
-    }" "https://texttospeech.googleapis.com/v1/text:synthesize" > synthesize-text.txt
-    echo `cat synthesize-text.txt | grep audioContent|cut -d ":" --fields=2|cut -d "\"" --fields=2` > synthesize-output-base64.txt
-    base64 synthesize-output-base64.txt --decode > ../abcs/$lang0/sounds/words/"$filename.$format"
-    rm -f synthesize-text.txt synthesize-output-base64.txt
-    ffmpeg -i ../abcs/$lang0/sounds/words/"$filename.$format" -acodec libvorbis ../abcs/$lang0/sounds/words/"$filename.$output"
-    if [ ! -f ../abcs/$lang0/sounds/words/"$filename.$output" ]
-    then
-      echo "Error while creating "$filename.$output""
-      rm -f ../abcs/$lang0/sounds/words/*.$format
-      exit 1
-    fi
+    get_sound "words"
 fi
 
 #clean up
